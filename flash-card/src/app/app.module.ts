0d106c428ec08baafd9253e77f700cf3759434eb
Dhiraj Kumar <31141888+dhiraj262@users.noreply.github.com>
2021-06-08 00:26:16 +0530

    image card

--- flash-card/src/app/app.module.ts
+++ flash-card/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlashCardComponent } from './components/flash-card/flash-card.component';
 import { Card1Component } from './components/card1/card1.component';
+import { ImageCardComponent } from './components/image-card/image-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FlashCardComponent,
-    Card1Component
+    Card1Component,
+    ImageCardComponent
   ],
   imports: [
     BrowserModule,
